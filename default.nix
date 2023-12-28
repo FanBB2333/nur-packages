30875479923385433632063e1e4b0b93f18273a4
L1ght <2504082957@qq.com>
2023-12-28 16:14:30 +0800

    remove unused pkgs

--- default.nix
+++ default.nix
@@ -14,9 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   mos = pkgs.callPackage ./pkgs/mos { };
-  hello-nur = pkgs.callPackage ./pkgs/hello-nur {};
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
